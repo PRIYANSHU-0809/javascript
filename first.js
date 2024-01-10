1ccb064aec85a02248842b85507c33137af727a7
PRIYANSHU KUMAR <92008349+PRIYANSHU-0809@users.noreply.github.com>
2024-01-10 15:37:43 +0530

    added_functions

---
+++ first.js
@@ -0,0 +1,48 @@
+// function printMsg(){
+
+//     alert("don't touch me");
+// }
+
+// // this is a comment
+
+// let a=3;
+
+// console.log(a);
+
+// let calculation= 2+2;
+// console.log(calculation);
+
+
+let cnt=0;
+
+function showQuantity(){
+    console.log(`Cart quantity: ${cnt}`);
+    
+}
+
+function addToCart(){
+    cnt+=1;
+    console.log(`Cart quantity: ${cnt}`);
+}
+
+function removeFromCart(){
+    cnt-=1;
+    console.log(`Cart quantity: ${cnt}`);
+}
+
+function addTwoElements(){
+    cnt+=2;
+    console.log(`Cart quantity: ${cnt}`);
+
+}
+
+function addThreeElements(){
+    cnt+=3;
+    console.log(`Cart quantity: ${cnt}`);
+}
+
+function resetCart(){
+    cnt=0;
+    console.log(`Cart quantity: ${cnt}`);
+    
+}
